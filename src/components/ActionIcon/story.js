2641a549960fa069340b5b0a8b5a31705fd6d495
Byron Mitchell <byron.alleman@gmail.com>
2017-08-15 18:41:45 -0400

    Added storybook info addon

--- src/components/ActionIcon/story.js
+++ src/components/ActionIcon/story.js
@@ -2,25 +2,27 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info';
+
 import ActionIcon from '../ActionIcon'
 import ActionTooltip from '../ActionTooltip'
 
 
 storiesOf('Action Icons', module)
-.add('Simple Action', () => {
-  const action = {
-    "cast" : "Instant",
-    "category" : "Job Actions",
-    "icon" : "icons/actions/astrologian_Job Actions_action__07.png",
-    "job" : "astrologian",
-    "name" : "Essential Dignity",
-    "recast" : "40s",
-    "tooltip" : "Restores target's HP.<br>Cure Potency: 400<br>Potency increases the lower the target's HP."
-  }
+  .add('Simple Action', withInfo()(() => {
+    const action = {
+      "cast": "Instant",
+      "category": "Job Actions",
+      "icon": "icons/actions/astrologian_Job Actions_action__07.png",
+      "job": "astrologian",
+      "name": "Essential Dignity",
+      "recast": "40s",
+      "tooltip": "Restores target's HP.<br>Cure Potency: 400<br>Potency increases the lower the target's HP."
+    }
 
-  return (
+    return (
       <ActionTooltip description={action.tooltip} cast={action.cast} recast={action.recast} name={action.name}>
-        <ActionIcon icon={action.icon} draggable={true}/>
+        <ActionIcon icon={action.icon} draggable={true} />
       </ActionTooltip>
-  )
-})
+    )
+  }))
--- src/components/JobIcon/story.js
+++ src/components/JobIcon/story.js
@@ -2,31 +2,33 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info';
+
 import JobIcon from '../JobIcon'
 
 import { JOB_ORDER } from '../../constants'
 storiesOf('Job Icons', module)
-.add('Simple Icons, no interactivity', () =>
-  <div>
-    {JOB_ORDER.map((job) => (
-      <JobIcon job={job} />
-    ))}
-  </div>
-)
-.add('Simple Icons, simple actions', () =>
-  <div>
-    {JOB_ORDER.map((job) => (
-      <JobIcon job={job} onClick={action('clicked')} />
-    ))}
-  </div>
-)
-.add('A single selected element', () => {
-
-  return (
+  .add('Simple Icons, no interactivity', withInfo()(() =>
     <div>
       {JOB_ORDER.map((job) => (
-        <JobIcon job={job} isSelected={job === 'astrologian'} />
+        <JobIcon job={job} />
       ))}
     </div>
-  )
-})
+  ))
+  .add('Simple Icons, simple actions', withInfo()(() =>
+    <div>
+      {JOB_ORDER.map((job) => (
+        <JobIcon job={job} onClick={action('clicked')} />
+      ))}
+    </div>
+  ))
+  .add('A single selected element', withInfo()(() => {
+
+    return (
+      <div>
+        {JOB_ORDER.map((job) => (
+          <JobIcon job={job} isSelected={job === 'astrologian'} />
+        ))}
+      </div>
+    )
+  }))
